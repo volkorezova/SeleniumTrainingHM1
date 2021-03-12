d5caefbbfc5903eaa33ff9f392a1d1ff7613b269
Tatyana Volkorezova <tanya.volkorezova@gmail.com>
2021-03-12 09:19:28 +0200

    Change logic of windows openning test (open, switch, close, switch to the base window)

--- src/test/java/HW3/OpenLinks.java
+++ src/test/java/HW3/OpenLinks.java
@@ -4,13 +4,18 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import utils.GeneralAction;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class OpenLinks extends GeneralActionsWithListener {
+    final int INCREMENT_OF_OPENED_WINDOWS = 1;
     String originalWindow;
     List<WebElement> externalLinksList;
+    Set<String> windowsBeforeNewOpened;
+    ArrayList<String> windowsAfterNewOpened;
 
     @Test
     void openWindowsAndClose(){
@@ -27,22 +32,20 @@ public class OpenLinks extends GeneralActionsWithListener {
 
         originalWindow = edr.getWindowHandle();
         externalLinksList = edr.findElements(externalLinks);
+        windowsBeforeNewOpened = edr.getWindowHandles();
 
         //open windows
         for (int i = 0; i < externalLinksList.size(); i++){
             externalLinksList.get(i).click();
-        }
+            wait.until(ExpectedConditions.numberOfWindowsToBe(windowsBeforeNewOpened.size() + INCREMENT_OF_OPENED_WINDOWS));
+            Assertions.assertEquals(windowsBeforeNewOpened.size() + INCREMENT_OF_OPENED_WINDOWS, edr.getWindowHandles().size());
+            windowsAfterNewOpened = new ArrayList<String>(edr.getWindowHandles());
+            edr.switchTo().window(windowsAfterNewOpened.get(INCREMENT_OF_OPENED_WINDOWS));
+            edr.close();
 
-        //close all tabs except first window (Countries)
-        for (String handl : edr.getWindowHandles()){
-            if (!handl.equals(originalWindow)) {
-                edr.switchTo().window(handl);
-                edr.close();
-            }
+            //switch to the original window
+            edr.switchTo().window(originalWindow);
         }
-
-        //switch to the original window
-        edr.switchTo().window(originalWindow);
         Assertions.assertTrue(isElementPresent(panelHeadingLocator), "Heading not found");
     }
 
